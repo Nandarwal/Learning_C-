0a55e6006308a666e301eb3710ac18b0c044e8a1
nandarwal <nandiniagbooks@gmail.com>
2024-07-12 21:13:40 +0530

    adding to github

--- halfpyramid.cpp
+++ halfpyramid.cpp
@@ -8,8 +8,15 @@ int main(){
 
     for (int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            cout<<" ";
+            if(j<=n-i){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+            
         }
+        cout<<endl;
     }
 
     return 0;
